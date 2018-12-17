87567a7b84ceda8f89dd34f0f653357dd4a4f757
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-17 15:38:31 -0800

    add search results component

---
+++ src/SearchResults.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import AlbumCover from './AlbumCover';
+
+import './styles/SearchResults.scss';
+
+function SearchResults() {
+  return (
+    <div className="mainBg">
+      i am a search result
+    </div>
+  )
+}
+
+export default SearchResults;
