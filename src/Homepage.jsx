830f7c590c54c109dee0ef2dc677a7f8d792fd19
Student <example@example.com>
2018-12-17 13:12:56 -0800

    add css grid with dummy data

---
+++ src/Homepage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import dopesmoker from './img/dopesmoker.jpg';
+
+import './styles/Homepage.scss';
+
+console.log(dopesmoker);
+
+function Homepage() {
+  return (
+    <div className="mainBg">
+      <div className="albumGrid">
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+        <img src={dopesmoker} alt="dopesmoker by sleep"></img>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
