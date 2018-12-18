37ca8e1d719860452154a6cf8e6172fca1144af1
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-18 08:53:04 -0800

    clean up code to satisfy linter

--- src/AlbumCover.jsx
+++ src/AlbumCover.jsx
@@ -8,7 +8,7 @@ import './styles/AlbumCover.scss';
 function AlbumCover() {
   return (
     <div className="albumCover">
-      <img src={dopesmoker}></img>
+      <img src={dopesmoker} alt="the album cover art for Dopesmoker by Sleep"></img>
     </div>
   )
 }
--- src/MagnifyingGlass.jsx
+++ src/MagnifyingGlass.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SimpleLineIcon from 'react-simple-line-icons';
 
 import './styles/MagnifyingGlass.scss';
