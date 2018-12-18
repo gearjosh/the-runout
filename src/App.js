72340561c5bbb40edef7a1a66f4d1f4aa7fcd3ea
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-17 16:16:39 -0800

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import './styles/normalize.css';
 import './styles/App.scss';
 
 import Header from './Header';
 import Homepage from './Homepage'
+import SearchResults from './SearchResults';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Homepage />
+        <Switch>
+          <Route exact path='/' component={Homepage}></Route>
+          <Route path='/searchresults' component={SearchResults}></Route>
+        </Switch>
       </div>
     );
   }
