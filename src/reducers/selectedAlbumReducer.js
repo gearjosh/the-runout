f1ce814cb61b3ee44dfacfbe770024fb160bada6
Student <example@example.com>
2018-12-19 14:34:18 -0800

    import initial state to reducers

--- src/reducers/selectedAlbumReducer.js
+++ src/reducers/selectedAlbumReducer.js
@@ -1,11 +1,11 @@
 import constants from './../constants';
-const { c } = constants;
+const { c, initialState } = constants;
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case c.SELECT_ALBUM:
-      return action.albumId;
-    default:
-      return state;
+  case c.SELECT_ALBUM:
+    return action.albumId;
+  default:
+    return state;
   }
 };
