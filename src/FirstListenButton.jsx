1216462d57f6eb1a088f552c157359f4b722e721
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-18 16:21:21 -0800

    create action buttons

---
+++ src/FirstListenButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import './styles/FirstListenButton.scss';
+
+function FirstListenButton() {
+  return (
+      <div className="actionElement actionElementEmpty unselectable">
+        First<br/>Time<br/>Listen?
+      </div>
+  );
+};
+
+export default FirstListenButton;
---
+++ src/LogAlbumButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import './styles/LogAlbumButton.scss';
+
+function LogAlbumButton() {
+  return (
+      <div className="actionElement actionElementFull unselectable">
+        Log
+        Album
+        Listen
+      </div>
+  );
+};
+
+export default LogAlbumButton;
