8fb003a2c3208601f43e9b15e0ae903716a97101
Student <example@example.com>
2018-12-19 15:51:08 -0800

    combine root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import selectedAlbumReducer from './selectedAlbumReducer';
 
 const rootReducer = combineReducers({
-  selectedAlbum: selectedAlbumReducer
+  selectedAlbum: selectedAlbumReducer,
+  listens: listenLogReducer
 });
 
 export default rootReducer;
