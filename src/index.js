740e2e6823716c0e3166b4baeba43aebf1519bc4
Student <example@example.com>
2018-12-19 13:22:52 -0800

    add commented-out redux logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import { createStore, /*(applyMiddleware }*/ from 'redux';
+// import { Provider } from 'react-redux';
+// import rootReducer from './reducers/index';
+// import thunkMiddleware from 'redux-thunk';
 
 import './styles/index.scss';
 
 import App from './App';
 
+// const store = createStore(rootReducer);
+
+// let unsubscribe = store.subscribe(() => {
+//   console.log(store.getState());
+// });
 
 const render = (Component) => {
   ReactDOM.render(
@@ -17,3 +26,5 @@ const render = (Component) => {
 };
 
 render(App);
+
+// add <Provider/> inside <HashRouter/> but outside <Component/> once store is up and running
