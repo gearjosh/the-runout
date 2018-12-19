e81250a7e8719b385749cd39069c645ec3b07bda
Student <example@example.com>
2018-12-19 10:30:49 -0800

    add user info component

---
+++ src/UserInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import './styles/UserInfo.scss';
+
+function UserInfo() {
+  return (
+    <div className="userInfo">
+
+    </div>
+  );
+};
+
+export default UserInfo;
+
+// <ProfileImage/>
