32fd6f96e09bc5f08a3c57a79963a7fbe80fc35d
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-18 16:21:07 -0800

    style genre tags

--- src/GenreTags.jsx
+++ src/GenreTags.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-// import './styles/GenreTags.scss';
+import './styles/GenreTags.scss';
 
 function GenreTags() {
   return (
     <div>
-      <span className="genreTag">Stoner Metal</span>
-      <span className="genreTag">Hip-Hop</span>
-      <span className="genreTag">Soul</span>
+      <span className="genreTag unselectable">Stoner Metal</span>
+      <span className="genreTag unselectable">Hip-Hop</span>
+      <span className="genreTag unselectable">Soul</span>
+      <button className="genreTag unselectable addGenreButton">+</button>
     </div>
   );
 };
