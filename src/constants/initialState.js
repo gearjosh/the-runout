6739b3f92338980aee012b28ee8109dd73132395
Student <example@example.com>
2018-12-19 14:33:38 -0800

    add initial state object

---
+++ src/constants/initialState.js
@@ -0,0 +1,14 @@
+export default {
+  selectedAlbum: 55625,
+  listens: [
+    {
+      1545248383000: {
+        albumId: 55625,
+        albumCover: './img/dopesmoker.jpg',
+        title: 'Dopesmoker',
+        artist: 'Sleep',
+        firstListen: false
+      },
+    }
+  ]
+};
