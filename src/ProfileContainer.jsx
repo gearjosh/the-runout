213cd94364549f4cb85049127fb1c16d1ad08d85
Student <example@example.com>
2018-12-19 10:31:41 -0800

    add profile container component

--- src/ProfileContainer.jsx
+++ src/ProfileContainer.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 
+import ProfileImage from './ProfileImage';
+import AlbumCover from './AlbumCover';
+import AlbumCover2 from './AlbumCover2';
+import AlbumCover3 from './AlbumCover3';
+import AlbumCover4 from './AlbumCover4';
+import AlbumCover5 from './AlbumCover5';
+
 import './styles/ProfileContainer.scss';
 
 function ProfileContainer() {
   return (
     <div className="profileContainer">
-      <h1>Ya Profile!</h1>
+
+      <div className="profileUserInfo">
+        <ProfileImage/>
+        <div>
+          <h1>[Your Name]</h1>
+          <h3>Location: [Where you live]</h3>
+          <ul>
+            <li>Albums Logged: [A number]</li>
+            <li>Albums Rated: [A number]</li>
+            <li>Albums Reviewed: [A number]</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="profileBody">
+        <h1>Your Recent Listens</h1>
+        <div className="listenLog">
+          <AlbumCover/>
+          <AlbumCover2/>
+          <AlbumCover3/>
+          <AlbumCover4/>
+          <AlbumCover5/>
+        </div>
+      </div>
+
     </div>
   );
 };
