a5707225a179156fe01764c2d6f8cdbff66435b3
Student <example@example.com>
2018-12-19 09:33:45 -0800

    add & style profile

---
+++ src/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles/Profile.scss';
+
+function Profile() {
+  return (
+    <div className="profileContainer">
+      <h1>Ya Profile!</h1>
+    </div>
+  );
+};
+
+export default Profile;
+
+// these will be the components to go here
+// <UserInfo/>
+// <ListenLog/>
