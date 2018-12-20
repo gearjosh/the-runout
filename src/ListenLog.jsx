d35fd7cd8454ec4bd30142519bf21f28c5e175f0
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-12-20 10:33:42 -0800

    update listen log with dynamic data

--- src/ListenLog.jsx
+++ src/ListenLog.jsx
@@ -4,19 +4,37 @@ import AlbumCoverWithInfo from './AlbumCoverWithInfo';
 
 import './styles/ListenLog.scss';
 
-function ListenLog() {
+function ListenLog(props) {
+
+  let bigListenArray = Object.keys(props.listens);
+  let smallListenArray = [];
+
+  function createSmallListenArray() {
+    for(let i = 0; i < 5; i ++) {
+      smallListenArray.push(bigListenArray[i]);
+    }
+  }
+  createSmallListenArray();
+
   return (
     <div>
       <h1>Your Recent Listens</h1>
       <div className="listenLog">
-        <AlbumCoverWithInfo/>
-        <AlbumCoverWithInfo/>
-        <AlbumCoverWithInfo/>
-        <AlbumCoverWithInfo/>
-        <AlbumCoverWithInfo/>
+        {smallListenArray.map((albumId) => {
+          let album = props.listens[albumId];
+          return <AlbumCoverWithInfo
+            albumId={albumId}
+            title={album.title}
+            artist={album.artist}
+            albumCover={album.albumCover}
+            firstListen={album.firstListen}
+            loggedAt={album.loggedAt}
+            releaseYear={album.releaseYear}
+            key={albumId} />
+        })}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default ListenLog;
+  export default ListenLog;
